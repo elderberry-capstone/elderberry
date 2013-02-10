a45c0f4b3f583342528b2b0bb9072794da9048ce
jm <jm.public@gmail.com>
2013-02-10 00:26:05 -0800

    Added simple signal handling.

--- av3-basic/fc.c
+++ av3-basic/fc.c
@@ -4,14 +4,23 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #include "libusb-basic.h"
 #include "logging.h"
 #include "miml.h"
 
 
+void signalhandler(int signum);
+void signalhandler(int signum) {
+	printf ("\n **signal handler: signum = %d", signum);
+}
+
 int main(int argc, char **argv)
 {
+	signal (SIGUSR1, signalhandler);
+	signal (SIGUSR2, signalhandler);
+
 	init_logging();
 
 	libusbSource * usb_source = libusbSource_new();
