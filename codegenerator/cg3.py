3140d592897b10b512e5ce237f130e59d97a792e
rastin <rastin@rastin-Latitude-E5420.(none)>
2013-02-18 16:22:15 -0800

    Checking in Object Oriented version of Code Generator

--- codegenerator/cg3.py
+++ codegenerator/cg3.py
@@ -2,38 +2,52 @@
 
 import yaml
 
-parse_handlers = {}
-path = ['']
+output_document = []
 
-def makepath():
-    return '/'.join(path)
+class ParsePath:
 
-def build_router():
-    return yaml.load(open('./cg.conf'))
+    def __init__(self):
+        self.path = ['']
 
-def matchpath(s):
-    for key in parse_handlers.keys():
-        if makepath() == parse_handlers[key]['path']:
-            eval(key)(s)
+    def append(self, token):
+        self.path.append(token)
 
-def parse(s):
-    if type(s).__name__=='dict':
-        for key in s.keys():
-            path.append(key)
-            matchpath(s)
-            parse (s[key])
-            path.pop()
-    elif type(s).__name__=='list':
-        for element in s:
-            matchpath(s)
-            parse (element)            
-    else:
-        matchpath(s)
-    #    print (makepath(), s)
+    def pop(self):
+        self.path.pop()
+
+    def getPath(self):
+        return '/'.join(self.path)
+
+class ParseRouter:
+
+    def __init__(self, filename):
+        self.parse_handlers = yaml.load(open(filename))
+        self.path = ParsePath()
+
+    def parse(self, s):
+        if type(s).__name__=='dict':
+            for key in s.keys():
+                self.path.append(key)
+                if self.matchpath(s[key]) == False:
+                    self.parse (s[key])
+                self.path.pop()
+        elif type(s).__name__=='list':
+            for element in s:
+                if self.matchpath(element) == False:
+                    self.parse (element)            
+        else:
+            self.matchpath(s)
+
+    def matchpath(self, s):
+        for key in self.parse_handlers.keys():
+            if self.path.getPath() == self.parse_handlers[key]['path']:
+                eval(key)(s)
+                return True
+        return False
 
 def parse_error(s):
     print ("Func parse_error", s)
-
+    
 def parse_finalize(s):
     print ("Func parse_finalize", s)
 
@@ -46,5 +60,8 @@ def parse_includes(s):
 def parse_initialize(s):
     print ("Func parse_initialize", s)
 
-parse_handlers = build_router()
-parse(yaml.load(open('./test.miml')))
+def parse_params(s):
+    print ("Func parse_params", s)
+
+parser = ParseRouter('./cg.conf')
+parser.parse(yaml.load(open('./test.miml')))
