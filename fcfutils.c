412c976db0b23d21a4dd9d64622f5c180527ae6e
mooper <mooper.hooper@gmail.com>
2013-03-05 17:21:49 -0800

    added new doxyfile, fcfutils formatting, and new html structure.

--- fcfutils.c
+++ fcfutils.c
@@ -26,13 +26,13 @@ struct fcffd{
 };
 
 static const char STANDARD = 0;	//< Standard callback
-static const char PPC = 1;		//< Per poll cycle callback
+static const char PPC = 1;	//< Per poll cycle callback
 
-static struct pollfd * fds;		//< File descriptor array
-static struct fcffd  * fdx;		//< File description array
-static int nfds;				//< Number of file descriptors in arrays
-static int fd_array_size;		//< Allocated size of file descriptor array, fds
-static int run_fc;				//< Main loop is running true/false
+static struct pollfd * fds;	//< File descriptor array
+static struct fcffd  * fdx;	//< File description array
+static int nfds;		//< Number of file descriptors in arrays
+static int fd_array_size;	//< Allocated size of file descriptor array, fds
+static int run_fc;		//< Main loop is running true/false
 
 
 static void debug_fd (const char *msg, int i, struct pollfd *pfd);
@@ -42,12 +42,13 @@ static void debug_fd (const char *msg, int i, struct pollfd *pfd);
  */
 int init_fcf(){
   fd_array_size = FDS_INIT_SIZE;
+  //initializing both file descriptor arrays
   fds = (struct pollfd *) malloc(fd_array_size * sizeof(struct pollfd));
   fdx = (struct fcffd *) malloc(fd_array_size * sizeof(struct fcffd));
   // TODO: Need error checking code here for malloc calls
 	
-  nfds = 0;
-  run_fc = 0;
+  nfds = 0;  //< Number of file descriptors in array
+  run_fc = 0;  //< Main loop is running True/False
 
   return 0;
 }
@@ -65,180 +66,204 @@ static int expand_arrays(){
   printf("%d\n", fd_array_size);
 	
 
+  //increase size of fds array
   fds_temp = realloc(fds, fd_array_size * sizeof(struct pollfd));
   if(fds_temp == NULL){
     // TODO: Add necessary logging/abort codes
-    return -1;
+    return -1;  //if failed
   }
   fds = fds_temp;
 	
+  //increase size of fdx array
   fdx_temp = realloc(fdx, fd_array_size * sizeof(struct fcffd));
   if(fdx_temp == NULL){
     // TODO: Add necessary logging/abort codes
-    return -1;
+    return -1;  //if failed
   }
   fdx = fdx_temp;
 
   return 0;
 }
 
+/*
+ *    Add file descriptor into both fdx and fds arrays
+ */ 
 int fcf_add_fd(int fd, short events, pollfd_callback cb){
-	// Checks to see if fd arrays are full
-	if(fd_array_size == nfds){
-		expand_arrays();
-	}
-
-	fds[nfds].fd = fd;
-	fds[nfds].events = events;
-	//fdx[nfds].token = token;
-	fdx[nfds].callback = cb;
-	fdx[nfds].cb_cat = STANDARD;
-	nfds++;
-	printf("Added fd: %d\tevents: %d\tFD count: %d\n", fd, events, nfds);
-	//printf("Added %s\tfd: %d\tevents: %d\tFD count: %d\n", token, fd, events, nfds);
-	return nfds-1;
+  // Checks to see if fd arrays are full, if they are expand arrays.
+  if(fd_array_size == nfds){
+    expand_arrays();
+  }
+  
+  //Filling file descriptor array with information bassed through function parameters.
+  fds[nfds].fd = fd;
+  fds[nfds].events = events;
+  //fdx[nfds].token = token;
+  fdx[nfds].callback = cb;
+  fdx[nfds].cb_cat = STANDARD;
+  nfds++;
+  printf("Added fd: %d\tevents: %d\tFD count: %d\n", fd, events, nfds);
+  //printf("Added %s\tfd: %d\tevents: %d\tFD count: %d\n", token, fd, events, nfds);
+  return nfds - 1; //return value is the index of the newest file descriptor
 }
 
 
-//add per poll cycle file descriptor
-int fcf_add_fd_ppc (/*const char *token,*/ int fd, short events, pollfd_callback cb)
-{
-	int i = fcf_add_fd (/*token,*/ fd, events, cb);
-	if (i >= 0) {
-		fdx[i].cb_cat = PPC;
-	}
-	return i;
+/*
+ *    Per poll loop file descriptor add
+ */
+int fcf_add_fd_ppc(/*const char *token,*/ int fd, short events, pollfd_callback cb){
+  int i = fcf_add_fd (/*token,*/ fd, events, cb);
+  if(i >= 0){
+    fdx[i].cb_cat = PPC;
+  }
+  return i;
 }
 
-
-void fcf_remove_fd(int fd) {
+/*
+ *    Removes the indicated file descriptor from the arrays.
+ */
+void fcf_remove_fd(int fd){
  
-	// If there are no fds, return 0 -- or error code.
-	if(nfds <= 0)
-		return;
+  // If there are no fds, return 0 -- or error code.
+  if(nfds <= 0)
+    return;
 
-	int i = 0;
+  int i = 0;
 	
-	for(i=0; i<nfds; i++){
-		if(fds[i].fd == fd && i==(nfds-1)){
-			//printf("Removed %s\tfd: %d\tevents: %d\tFD count: %d\n", fdx[i].token, fds[i].fd, fds[i].events, nfds-1);
-			nfds--;
-		}
-		else if(fds[i].fd == fd){
-			//printf("Removed %s\tfd: %d\tevents: %d\tFD count: %d\n", fdx[i].token, fds[i].fd, fds[i].events, nfds-1);
-			memmove(&fds[i], &fds[nfds - 1], sizeof(struct pollfd));
-			memmove(&fdx[i], &fdx[nfds - 1], sizeof(struct fcffd));
-			nfds--;
-		}
-	}
+  for(i = 0; i < nfds; i++){
+    if(fds[i].fd == fd && i == (nfds - 1)){
+      //printf("Removed %s\tfd: %d\tevents: %d\tFD count: %d\n", fdx[i].token, fds[i].fd, fds[i].events, nfds - 1);
+      nfds--;
+    }
+    else if(fds[i].fd == fd){
+      //printf("Removed %s\tfd: %d\tevents: %d\tFD count: %d\n", fdx[i].token, fds[i].fd, fds[i].events, nfds-1);
+      memmove(&fds[i], &fds[nfds - 1], sizeof(struct pollfd));
+      memmove(&fdx[i], &fdx[nfds - 1], sizeof(struct fcffd));
+      nfds--;
+    }
+  }
 }
 
+/*
+ *    Function returns file descriptor information for specified array index
+ */
 struct pollfd *fcf_get_fd(int idx){
   return &fds[idx];
 }
 
+/*
+ *    Stops main poll loop from running
+ */
 void fcf_stop_main_loop(){
   run_fc = 0;
 }
 
+/*
+ *    Starts main poll loop running
+ */
 static void fcf_start_main_loop(){
   run_fcc = 1;
 }
 
-//currently returns -1 on error; 0 on success
-static int fcf_run_poll_loop() {
-	pollfd_callback ppc[nfds];
-	int nppc= 0;
-	//int count = 0;
-	int ret = 0;
+/*
+ *    Detects change in file descriptors and polls fd arrays
+ */
+static int fcf_run_poll_loop(){
+  //currently returns -1 on error; 0 on success.
+  pollfd_callback ppc[nfds];
+  int nppc = 0, ret = 0;
+  //int count = 0;
 
-	fcf_start_main_loop();
-	while (run_fc) {
+  fcf_start_main_loop();
+  while(run_fc){
 
-		/*for (int i = 0; i < nfds; i++) {
-			debug_fd("\nbefore poll<<< ", i, &fds[i]);
-		}*/
-		//printf("\nwaiting");
-		fflush (stdout);
+    /*for (int i = 0; i < nfds; i++) {
+      debug_fd("\nbefore poll<<< ", i, &fds[i]);
+      }*/
+    //printf("\nwaiting");
+    fflush(stdout);
 
-		errno = 0;
-		int rc = poll(fds, nfds, -1);
+    errno = 0;
+    int rc = poll(fds, nfds, -1);
 
-		//printf ("\n%d. poll returned with rc=%d errno=%d", count++, rc, errno);
-		/*for (int i = 0; i < nfds; i++) {
-			debug_fd("\n   after poll>>> ", i, &fds[i]);
-		}*/
+    //printf ("\n%d. poll returned with rc=%d errno=%d", count++, rc, errno);
+    /*for (int i = 0; i < nfds; i++) {
+      debug_fd("\n   after poll>>> ", i, &fds[i]);
+      }*/
 
-		switch (rc) {
-		case -1: //error
-			if (errno != EINTR) {
-				perror ("run_main_loop: poll returned with error");
-				ret = -1;
-				fcf_stop_main_loop();
-			}
-			break;
-		case 0: //timeout
-			printf("poll timed out");
-			break;
-		default:
-			nppc = 0;
-			for (int i = 0; i < nfds && rc > 0; i++) {
-				if(fds[i].revents != 0) {
-					debug_fd("\n active fd ", i, &fds[i]);
-					rc--;
-					if (fdx[i].cb_cat == STANDARD) {
-						//callback for this active fd is a standard callback
-						fdx[i].callback(&fds[i]);
-					} 
-					else {
-						//callback for this active fd is a "per poll cycle" callback
-						int j;
-						for (j = 0; j < nppc && ppc[j] != fdx[i].callback; j++)
-						{ /*empty*/ }
-						if (j == nppc) {
-							//a new ppc callback
-							//add to ppc so that callback will be called at end of poll cycle
-							ppc[nppc++] = fdx[i].callback;
-						} else {
-							//we have seen this callback before
-							printf ("\n multiple active ppc, ignoring callback for fd[%d]: fd=%d", i, fds[i].fd);
-						}
-					}
-				} // (revents set)
-			} // (for i)
+    switch (rc){
+    case -1: //error
+      if(errno != EINTR){
+	perror ("run_main_loop: poll returned with error");
+	ret = -1;
+	fcf_stop_main_loop();
+      }
+      break;
+    case 0: //timeout
+      printf("poll timed out");
+      break;
+    default:
+      nppc = 0;
+      for(int i = 0; i < nfds && rc > 0; i++){
+	if(fds[i].revents != 0){
+	  debug_fd("\n active fd ", i, &fds[i]);
+	  rc--;
+	  if(fdx[i].cb_cat == STANDARD){
+	    //callback for this active fd is a standard callback
+	    fdx[i].callback(&fds[i]);
+	  } 
+	  else{
+	    //callback for this active fd is a "per poll cycle" callback
+	    int j;
+	    for(j = 0; j < nppc && ppc[j] != fdx[i].callback; j++)
+	      { /*empty*/ }
+	    if(j == nppc){
+	      //a new ppc callback
+	      //add to ppc so that callback will be called at end of poll cycle
+	      ppc[nppc++] = fdx[i].callback;
+	    } 
+	    else{
+	      //we have seen this callback before
+	      printf("\n multiple active ppc, ignoring callback for fd[%d]: fd=%d", i, fds[i].fd);
+	    }
+	  }
+	} // (revents set)
+      } // (for i)
 
-			//handle ppc callbacks
-			for (int j = 0; j < nppc; j++) {
-				//if callback wants to access the fds, callback
-				//is expected to know the indices into the fds array
-				//i.e., module must store return values it gets from fcf_addfdPpc
-				printf ("\n calling ppc callback [%d]", j);
-				ppc[j](fds);
-			}
+      //handle ppc callbacks
+      for(int j = 0; j < nppc; j++){
+	//if callback wants to access the fds, callback
+	//is expected to know the indices into the fds array
+	//i.e., module must store return values it gets from fcf_addfdPpc
+	printf("\n calling ppc callback [%d]", j);
+	ppc[j](fds);
+      }
 
-			break;
-		}
+      break;
+    }
 
-	}
+  }
 
-	printf("\n exiting main loop");
-	return ret;
+  printf("\n exiting main loop");
+  return ret;
 }
 
-static void debug_fd (const char *msg, int i, struct pollfd *pfd) {
-	//printf("%s fd[%d]: fd=%d events=%X revents=%X [%s]", msg, i, pfd->fd, pfd->events, pfd->revents, fdx[i].token);
-	int re = pfd->revents;
-	if (re & POLLERR) printf("\nPOLLERR - Error condition");
-	if (re & POLLHUP) printf("\nPOLLHUP - Hang up");
-	if (re & POLLNVAL) printf("\nPOLLNVAL - Invalid request: fd not open");
-	fflush (stdout);
+/*
+ *    Prints out polling information
+ */
+static void debug_fd (const char *msg, int i, struct pollfd *pfd){
+  //printf("%s fd[%d]: fd=%d events=%X revents=%X [%s]", msg, i, pfd->fd, pfd->events, pfd->revents, fdx[i].token);
+  int re = pfd->revents;
+  if(re & POLLERR) printf("\nPOLLERR - Error condition");
+  if(re & POLLHUP) printf("\nPOLLHUP - Hang up");
+  if(re & POLLNVAL) printf("\nPOLLNVAL - Invalid request: fd not open");
+  fflush(stdout);
 }
 
 
 static void signalhandler(int signum){
   printf ("\n **signal handler: signum = %d", signum);
 
-  if (signum == SIGINT){
+  if(signum == SIGINT){
     fcf_stop_main_loop ();
   }
 }
@@ -248,16 +273,16 @@ int main(int argc, char *argv[]){
   
   printf("Flight Control Framework v0.1  Copyright (C) 2013\n"
 	 "Team Elderberry [Ron Astin, Clark Wachsmuth, Chris Glasser, Josef Mihalits, Jordan Hewitt, Michael Hooper]\n"
-	 "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n"
+	 "This program comes with ABSOLUTELY NO WARRANTY; for details please visit ...... .\n"
 	 "This is free software, and you are welcome to redistribute it\n"
-	 "under certain conditions; type `show c' for details.\n");
+	 "under certain conditions; Please visit ........................... for details.\n");
   signal (SIGINT, signalhandler);
 
-	fcf_init();
-	int rc = fcf_run_poll_loop();
+  fcf_init();
+  int rc = fcf_run_poll_loop();
 
-	if (rc == 0) {
-		return EXIT_SUCCESS;
-	}
-	return EXIT_FAILURE;
+  if(rc == 0) {
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE;
 }
