6fa16ec3becafdefd70990824ff849d60a1b507b
mooper <mooper.hooper@gmail.com>
2013-03-08 18:01:37 -0800

    fcfutils modifications. adding parameters and licensing info

--- fcfutils.h
+++ fcfutils.h
@@ -3,16 +3,18 @@
  * @brief Utility functions for the flight control framework (fcf)
  * @details The core framework file, it starts in the main function, introducing the rogram.  This program has signal handlers for when the user exits abrubtly using ctrl+Z, allowing a graceful shutdown of the software.  The main function also initializes the framework and the modules before running the main loop. Lastly, this file contains the struct that holds the callback functions and source tokens of each device.  
 
-Below are the variables for this file:
+<<<<<<<<<<<<<<<<<<<< LICENSING INFORMATION >>>>>>>>>>>>>>>>>>>>
+\n FLIGHT CONTROL FRAMEWORK V0.1  Copyright (C) 2013\n
+Ron Astin, Clark Wachsmuth, Chris Glasser, Josef Mihalits, Jordan Hewitt, Michael Hooper]\n\n
+----------------------------------------------------------------\n
+This program comes with ABSOLUTELY NO WARRANTY;\n for details please
+visit http://www.gnu.org/licenses/gpl.html.\n\n
+This is free software, and you are welcome to redistribute it\n
+under certain conditions; For details, please visit\n
+http://www.gnu.org/licenses/gpl.html.\n
+----------------------------------------------------------------\n\n\n
 
- static const char STANDARD = 0;//< Standard callback\n
- static const char PPC = 1;//< Per poll cycle callback\n
- static struct pollfd * fds;//< File descriptor array\n
- static struct fcffd  * fdx;//< File description array\n
- static int nfds;//< Number of file descriptors in arrays\n
- static int fd_array_size;//< Allocated size of file descriptor array, fds\n
- static int run_fc;//< Main loop is running true/false\n
-
+                                           
  * @author Team Elderberry
  * @date March 2nd, 2013
  */
@@ -30,14 +32,14 @@ typedef void (*pollfd_callback)(struct pollfd *);
  * @details Checks to see if the file descriptor arrays are full.  If the arrays are full it calls the expand_arrays() fucntion (this will double the size of the arrays). It adds information to two arrays, the fds and fdx arrays.  The fds array has pollfd pointers (required by the poll system call) and the fdx array has fcffd pointers (required by our framework [containing callback functions and other information])
  * @return index of the last fd
  */
-extern int fcf_add_fd(int, short, pollfd_callback);
+extern int fcf_add_fd(int fd, short events, pollfd_callback cb);
 /**
  * @brief simply removes a specified file descriptor from the arrays
  * @details If there are no fds in the array, the function errors out. If there are fd's in the arrays they are removed from both the fds and fdx arrays.  
  */
-extern void fcf_remove_fd(int);
+extern void fcf_remove_fd(int fd);
 
-extern struct pollfd * fcf_get_fd(int);
+extern struct pollfd * fcf_get_fd(int idx);
 /**
  * @brief stops main loop by setting run_fc to 0
  */
